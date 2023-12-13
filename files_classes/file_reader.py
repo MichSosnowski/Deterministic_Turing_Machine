5a7a6684056a8f597b1f2cb9256ecac3ce7412b6
Michał Sosnowski <michaks@wp.pl>
2023-12-13 20:54:37 +0100

    Added some annotations

--- files_classes/file_reader.py
+++ files_classes/file_reader.py
@@ -3,23 +3,23 @@ import constants.constants as constants
 class FileReader:
 
     def __init__(self, filename: str) -> None:
-        self.filename = filename
+        self.filename: str = filename
         self.check_file_exists()
 
-    def check_file_exists(self):
+    def check_file_exists(self) -> None:
         try:
             open(self.filename).close()
         except FileNotFoundError:
             raise FileNotFoundError
 
     def get_all_data_from_file(self) -> str:
-        data = constants.EMPTY_STRING
+        data: str = constants.EMPTY_STRING
         with open(self.filename) as file:
-            data = file.read()
+            data: str = file.read()
         return data
 
     def get_entry_word_from_file(self) -> str:
         with open(self.filename) as file:
-            lines = file.readlines()
-        search_line = lines.index(constants.SEARCH_LINE)
+            lines: list[str] = file.readlines()
+        search_line: str = lines.index(constants.SEARCH_LINE)
         return lines[search_line + constants.NEXT_INDEX][:constants.LAST_CHAR_INDEX]
