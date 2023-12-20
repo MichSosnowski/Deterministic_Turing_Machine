3afeae9250c655f8305655422d2e28d84af0b01f
Michał Sosnowski <michaks@wp.pl>
2023-12-20 22:02:46 +0100

    Added annotations for constants in constants.py

--- constants/constants.py
+++ constants/constants.py
@@ -1,87 +1,85 @@
 # constants used in file_reader.py, mainwindow.py and turing_machine.py
-EMPTY_STRING = ''
+EMPTY_STRING: str = ''
 
 
 # constants used in file_reader.py and file_parser.py
-ENTRY_WORD = 'slowo wejsciowe:\n'
-INITIAL_STATE = 'stan poczatkowy:\n'
-ACCEPTING_STATES = 'stany akceptujace:\n'
-TRANSITION_FUNCTION = 'relacja przejscia:\n'
+ENTRY_WORD: str = 'slowo wejsciowe:\n'
+INITIAL_STATE: str = 'stan poczatkowy:\n'
+ACCEPTING_STATES: str = 'stany akceptujace:\n'
+TRANSITION_FUNCTION: str = 'relacja przejscia:\n'
 
 
 # constants used in file_reader.py, file_parser.py and turing_machine.py
-LAST_CHAR_INDEX = -1
-SPACE = ' '
+LAST_CHAR_INDEX: int = -1
+SPACE: str = ' '
 
 
 # constants used in file_parser.py and turing_machine.py
-EMPTY_CHAR = '#'
-LEFT = 'L'
-RIGHT = 'P'
+EMPTY_CHAR: str = '#'
+LEFT: str = 'L'
+RIGHT: str = 'P'
 
 
 # constants used in turing_machine.py
-INITIAL_TAPE_SIZE = 32
-DRAWN_TAPE_SIZE = 19
-FIRST_TAPE_INDEX = 0
-EMPTY_CHAR_ENTRY_WORD_POSITION = 10
-INITIAL_FRAGMENT_POSITION_BACK = 9
-END_FRAGMENT_POSITION_FORWARD = 10
-INITIAL_CALCULATION_LENGTH = 0
-PREVIOUS_CELL = -1
-NEXT_CELL = 1
-CALCULATION_LENGTH_INCREASE = 1
-THREAD_SLEEP_SECS = 0.25
+INITIAL_TAPE_SIZE: int = 32
+DRAWN_TAPE_SIZE: int = 19
+FIRST_TAPE_INDEX: int = 0
+EMPTY_CHAR_ENTRY_WORD_POSITION: int = 10
+INITIAL_FRAGMENT_POSITION_BACK: int = 9
+END_FRAGMENT_POSITION_FORWARD: int = 10
+INITIAL_CALCULATION_LENGTH: int = 0
+PREVIOUS_CELL: int = -1
+NEXT_CELL: int = 1
+CALCULATION_LENGTH_INCREASE: int = 1
+THREAD_SLEEP_SECS: float = 0.25
 
 
 # constants used in file_reader.py
-NEXT_INDEX = 1
+NEXT_INDEX: int = 1
 
 
 # constants used in file_parser.py
-TAPE_ALPHABET = 'alfabet tasmowy:\n'
-ENTRY_ALPHABET = 'alfabet wejsciowy:\n'
-STATES = 'stany:\n'
-NEWLINE = '\n'
-CHAR_NOT_FOUND = -1
-REQUIRED_COUNT = 1
-REQUIRED_COUNT_TAPE_ALPHABET = 2
-MAX_LENGTH_ENTRY_WORD = 30
+TAPE_ALPHABET: str = 'alfabet tasmowy:\n'
+ENTRY_ALPHABET: str = 'alfabet wejsciowy:\n'
+STATES: str = 'stany:\n'
+NEWLINE: str = '\n'
+CHAR_NOT_FOUND: int = -1
+REQUIRED_COUNT: int = 1
+REQUIRED_COUNT_TAPE_ALPHABET: int = 2
+MAX_LENGTH_ENTRY_WORD: int = 30
 
 
 # constants used in mainwindow.py
-EXIT_SUCCESS = 0
-EXIT_FAILURE = -1
-WINDOW_TITLE = 'Maszyna Turinga'
-MESSAGE_TITLE_FORMAT = 'Niepoprawny format!'
-MESSAGE_FORMAT = 'Niepoprawny format pliku wejściowego.\nSprawdź format pliku i spróbuj ponownie.'
-MESSAGE_TITLE_END_PROGRAM = 'Zamknąć program?'
-MESSAGE_END_PROGRAM = 'Maszyna Turinga wciąż działa.\nCzy na pewno chcesz wyjść?'
-FILE_OPEN_TITLE = 'Wybierz plik...'
-CURRENT_DIR = '.'
-TXT_FILTER = 'Pliki tekstowe (*.txt)'
-FILENAME_INDEX = 0
-PIXMAP_BACKGROUND_COLOR = '#f0f0f0'
-BROWN = 'brown'
-PERU = 'peru'
-WIDTH_COEFFICIENT = 2/3
-HEIGHT_COEFFICIENT = 0.6
-BEG_POINT_X_RECT = 0
-BEG_POINT_Y_RECT_COEFFICIENT = 0.3
-END_POINT_Y_RECT = 45
-CELLS_LOCATION_HEIGHT_COEFFICIENT = BEG_POINT_Y_RECT_COEFFICIENT
-CELL_WIDTH_PEN = 32
-X_LOC_FIRST_CELL = 18
-Y_LOC_CELL_INCREASED = 23
-X_DIST_NEXT_CELL = 35
-HEAD_LOC_COEFFICIENT = 3
-HEAD_Y_BOTTOM_POINT_LOC_ABOVE_TAPE = 70
-HEAD_X_TOP_POINT_LOC_ABOVE_TAPE = 20
-HEAD_Y_TOP_POINT_LOC_ABOVE_TAPE = 100
-HEAD_WIDTH_PEN = 1
-FAMILY_FONT = 'Times'
-POINT_SIZE = 22
-FIRST_LETTER_POS = 6
-NEXT_LETTER_POS = 35
-LETTER_HEIGHT_COEFFICIENT = 0.354
-FILL_TABLE_NO_TRANSITION = ' '
+WINDOW_TITLE: str = 'Maszyna Turinga'
+MESSAGE_TITLE_FORMAT: str = 'Niepoprawny format!'
+MESSAGE_FORMAT: str = 'Niepoprawny format pliku wejściowego.\nSprawdź format pliku i spróbuj ponownie.'
+MESSAGE_TITLE_END_PROGRAM: str = 'Zamknąć program?'
+MESSAGE_END_PROGRAM: str = 'Maszyna Turinga wciąż działa.\nCzy na pewno chcesz wyjść?'
+FILE_OPEN_TITLE: str = 'Wybierz plik...'
+CURRENT_DIR: str = '.'
+TXT_FILTER: str = 'Pliki tekstowe (*.txt)'
+FILENAME_INDEX: int = 0
+PIXMAP_BACKGROUND_COLOR: str = '#f0f0f0'
+BROWN: str = 'brown'
+PERU: str = 'peru'
+WIDTH_COEFFICIENT: float = 2/3
+HEIGHT_COEFFICIENT: float = 0.6
+BEG_POINT_X_RECT: int = 0
+BEG_POINT_Y_RECT_COEFFICIENT: float = 0.3
+END_POINT_Y_RECT: int = 45
+CELLS_LOCATION_HEIGHT_COEFFICIENT: float = BEG_POINT_Y_RECT_COEFFICIENT
+CELL_WIDTH_PEN: int = 32
+X_LOC_FIRST_CELL: int = 18
+Y_LOC_CELL_INCREASED: int = 23
+X_DIST_NEXT_CELL: int = 35
+HEAD_LOC_COEFFICIENT: int = 3
+HEAD_Y_BOTTOM_POINT_LOC_ABOVE_TAPE: int = 70
+HEAD_X_TOP_POINT_LOC_ABOVE_TAPE: int = 20
+HEAD_Y_TOP_POINT_LOC_ABOVE_TAPE: int = 100
+HEAD_WIDTH_PEN: int = 1
+FAMILY_FONT: str = 'Times'
+POINT_SIZE: int = 22
+FIRST_LETTER_POS: int = 6
+NEXT_LETTER_POS: int = 35
+LETTER_HEIGHT_COEFFICIENT: float = 0.354
+FILL_TABLE_NO_TRANSITION: str = ' '
